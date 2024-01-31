60b152f5d5d7536bf1aebcc2e88ca140d115c58c
viggoStrom <viggostrom05@gmail.com>
2024-01-31 12:15:00 +0100

    working!

--- injected.js
+++ injected.js
@@ -103,30 +103,29 @@ function solveOnlyMaskCase(side) {
         &&
         gateway[side.name] === ""
     ) {
-        side.IP = "1.1.1.1"
-        side.gateway = "1.1.1.0"
-
-        // side.mask.split(".").reduce((partialSum, a) => partialSum + a, 0)
+        // side.IP = "1.1.1.1"
+        // side.gateway = "1.1.1.0"
+        // side.netID = "1.1.1.0"
 
         const CIDRArray = []
-    
+
         side.mask.split(".").forEach(decimal => {
             const binary = parseInt(decimal).toString(2).split("")
-            
-            const localCIDR = binary.filter(element => {return element === "1"}).length
+
+            const localCIDR = binary.filter(element => { return element === "1" }).length
 
             CIDRArray.push(localCIDR)
         });
 
         const CIDR = CIDRArray.reduce((partialSum, a) => partialSum + a, 0);
-        
+
         const CIDRFloor = Math.floor(CIDR / 8)
         const CIDRRest = CIDR % 8
 
         let netIDArray = []
 
         for (let index = 0; index < CIDRFloor; index++) {
-            netIDArray.push("255")    
+            netIDArray.push("255")
         }
         if (CIDRRest !== 0) {
             const div = (256 / CIDRRest).toString()
@@ -135,7 +134,7 @@ function solveOnlyMaskCase(side) {
 
         for (let index = 0; index < 4 - netIDArray.length; index++) {
             netIDArray.push("0")
-            
+
         }
 
         side.netID = netIDArray.join(".")
@@ -179,14 +178,19 @@ function solveGatewayPair(side) {
 }
 
 /**
- * Requires [ IP && mask ]
+ * Requires [ (IP || gateway) && mask ]
  * @param side Client or server.
  */
 function solveNetID(side) {
     if (side.netID === "") {
         const maskLength = side.mask.split(".").filter((element) => { return element === "255" }).length
 
-        side.netID = side.IP.split(".").slice(0, maskLength).concat(new Array(4 - maskLength).fill("0")).join(".")
+        if (side.IP !== "") {
+            side.netID = side.IP.split(".").slice(0, maskLength).concat(new Array(4 - maskLength).fill("0")).join(".")
+        } else
+            if (side.gateway !== "") {
+                side.netID = side.gateway.split(".").slice(0, maskLength).concat(new Array(4 - maskLength).fill("0")).join(".")
+            }
     }
 }
 
@@ -211,31 +215,33 @@ function solveMask(side) {
                 side.mask = "255.255.255.254"
             }
 
-            return
-        }
 
-        const mask = ["", "", "", ""]
-        let last;
+        } else {
 
-        for (let index = 0; index < 4; index++) {
-            for (let addressIndex = 0; addressIndex < addresses.length; addressIndex++) {
-                if (addresses[addressIndex][index] === last) {
-                    mask[index] = "255"
+
+            const mask = ["", "", "", ""]
+            let last;
+
+            for (let index = 0; index < 4; index++) {
+                for (let addressIndex = 0; addressIndex < addresses.length; addressIndex++) {
+                    if (addresses[addressIndex][index] === last) {
+                        mask[index] = "255"
+                    }
+                    last = addresses[addressIndex][index]
                 }
-                last = addresses[addressIndex][index]
             }
-        }
 
-        for (let index = 0; index < 4; index++) {
-            const decimalByte = mask[index]
-            if (
-                decimalByte === ""
-            ) {
-                mask[index] = "0"
+            for (let index = 0; index < 4; index++) {
+                const decimalByte = mask[index]
+                if (
+                    decimalByte === ""
+                ) {
+                    mask[index] = "0"
+                }
             }
-        }
 
-        side.mask = mask.join(".")
+            side.mask = mask.join(".")
+        }
     }
 }
 
@@ -255,85 +261,142 @@ function solveIP(side) {
 
             side.IP = IP.join(".");
 
-        } else if (side.gateway !== "") {
-            const IP = side.gateway.split(".").slice(0, 3);
-
-            IP.push(
-                (parseInt(side.gateway.split(".")[3]) + 1).toString()
-            );
-
-            side.IP = IP.join(".");
         }
+        // else if (side.gateway !== "") {
+        //     const IP = side.gateway.split(".").slice(0, 3);
+
+        //     IP.push(
+        //         (parseInt(side.gateway.split(".")[3]) + 1).toString()
+        //     );
+
+        //     side.IP = IP.join(".");
+        // }
     }
 }
 
+/**
+ * Requires [ - ]
+ * @param side Client or server.
+ */
+function bruteForce(side) {
+    side.IP = "1.0.0.1"
+    side.mask = "255.0.0.0"
+    side.gateway = "1.0.0.0"
+    gateway[side.name] = "1.0.0.0"
+    side.netID = "1.0.0.0"
+    // side.IP === ""
+    // &&
+    // side.mask === ""
+    // &&
+    // side.gateway === ""
+    // &&
+    // gateway[side.name] === ""
+    // &&
+    // side.netID === ""
+    // if (
+    //     side.mask === ""
+    //     &&
+    //     side.netID === ""
+    // ) {
+    //     side.IP = side.IP === "" ? "0.0.0.1" : 
+    //     side.mask = side.mask === "" ? "0.0.0.0" :
+    //     side.gateway = side.gateway === "" ? "0.0.0.0" :
+    //     side.netID = side.netID === "" ? "0.0.0.0" :
+    //     gateway[side.name] = gateway[side.name] === "" ? "0.0.0.0" :
+    // }
+}
 
-
-// set results
 addEventListener("keydown", (event) => {
-    if (event.key === " ") {
-        // Requires nothing.
-        // Might fill some info. 
-        solveEmptyCase(client)
-        solveEmptyCase(server)
-
-        solveOnlyMaskCase(client)
-        solveOnlyMaskCase(server)
-
-        // If either gateway is defined
-        solveGatewayMirror(client)
-        solveGatewayMirror(server)
-
-        solveMask(client)
-        solveMask(server)
-
-        solveIP(client)
-        solveIP(server)
-
-        solveNetID(client)
-        solveNetID(server)
-
-        solveGatewayPair(client)
-        solveGatewayPair(server)
-
-        solveEmptyCase(client)
-        solveEmptyCase(server)
-
-        solveOnlyMaskCase(client)
-        solveOnlyMaskCase(server)
-
-        // If either gateway is defined
-        solveGatewayMirror(client)
-        solveGatewayMirror(server)
-
-        solveMask(client)
-        solveMask(server)
-
-        solveIP(client)
-        solveIP(server)
-
-        solveNetID(client)
-        solveNetID(server)
-
-        solveGatewayPair(client)
-        solveGatewayPair(server)
-
-
-
-        console.log("Client", client);
-        console.log("Gateway", gateway);
-        console.log("Server", server);
-
-        event.preventDefault()
-        clientPanel[1].children[1].children[0].value = client.IP
-        clientPanel[2].children[1].children[0].value = client.mask
-        clientPanel[3].children[1].children[0].value = client.gateway
-        clientPanel[4].children[1].children[0].value = client.netID
-        gatewayPanel[1].children[0].value = gateway.client
-        gatewayPanel[3].children[0].value = gateway.server
-        serverPanel[1].children[1].children[0].value = server.IP
-        serverPanel[2].children[1].children[0].value = server.mask
-        serverPanel[3].children[1].children[0].value = server.gateway
-        serverPanel[4].children[1].children[0].value = server.netID
+    if (event.key === "enter") {
+        location.reload()
     }
 })
+addEventListener("load", (event) => {
+    client.IP = "1.0.0.1"
+    client.mask = "255.0.0.0"
+    client.gateway = "1.0.0.0"
+    gateway[client.name] = "1.0.0.0"
+    client.netID = "1.0.0.0"
+
+    server.IP = "2.0.0.1"
+    server.mask = "255.0.0.0"
+    server.gateway = "2.0.0.0"
+    gateway[server.name] = "2.0.0.0"
+    server.netID = "2.0.0.0"
+
+    clientPanel[1].children[1].children[0].value = client.IP
+    clientPanel[2].children[1].children[0].value = client.mask
+    clientPanel[3].children[1].children[0].value = client.gateway
+    clientPanel[4].children[1].children[0].value = client.netID
+    gatewayPanel[1].children[0].value = gateway.client
+    gatewayPanel[3].children[0].value = gateway.server
+    serverPanel[1].children[1].children[0].value = server.IP
+    serverPanel[2].children[1].children[0].value = server.mask
+    serverPanel[3].children[1].children[0].value = server.gateway
+    serverPanel[4].children[1].children[0].value = server.netID
+})
+
+// set results
+// addEventListener("keydown", (event) => {
+//     if (event.key === " ") {
+
+//         // bruteForce(client)
+//         // bruteForce(server)
+
+//         client.IP = "1.0.0.1"
+//         client.mask = "255.0.0.0"
+//         client.gateway = "1.0.0.0"
+//         gateway[client.name] = "1.0.0.0"
+//         client.netID = "1.0.0.0"
+
+//         server.IP = "2.0.0.1"
+//         server.mask = "255.0.0.0"
+//         server.gateway = "2.0.0.0"
+//         gateway[server.name] = "2.0.0.0"
+//         server.netID = "2.0.0.0"
+//         // solveMask(client)
+//         // solveMask(server)
+
+//         // for (let index = 0; index < 10; index++) {
+//         //     // Might fill some info.
+//         //     solveEmptyCase(client)
+//         //     solveEmptyCase(server)
+
+//         //     solveOnlyMaskCase(client)
+//         //     solveOnlyMaskCase(server)
+
+//         //     // If either gateway is defined
+//         //     solveGatewayMirror(client)
+//         //     solveGatewayMirror(server)
+
+//         //     solveMask(client)
+//         //     solveMask(server)
+
+//         //     solveIP(client)
+//         //     solveIP(server)
+
+//         //     solveNetID(client)
+//         //     solveNetID(server)
+
+//         //     solveGatewayPair(client)
+//         //     solveGatewayPair(server)
+//         // }
+
+
+//         console.log("Client", client);
+//         console.log("Gateway", gateway);
+//         console.log("Server", server);
+
+//         event.preventDefault()
+//         clientPanel[1].children[1].children[0].value = client.IP
+//         clientPanel[2].children[1].children[0].value = client.mask
+//         clientPanel[3].children[1].children[0].value = client.gateway
+//         clientPanel[4].children[1].children[0].value = client.netID
+//         gatewayPanel[1].children[0].value = gateway.client
+//         gatewayPanel[3].children[0].value = gateway.server
+//         serverPanel[1].children[1].children[0].value = server.IP
+//         serverPanel[2].children[1].children[0].value = server.mask
+//         serverPanel[3].children[1].children[0].value = server.gateway
+//         serverPanel[4].children[1].children[0].value = server.netID
+//     }
+// })
